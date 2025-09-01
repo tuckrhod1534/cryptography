fdf0a4ce1cd5e0e16d5fd396ec4c62a4f3314ef6
Tucker Rhodes <mrteachertuck@gmail.com>
2025-08-31 23:54:27 -0400

    Working through cryptography text book

---
+++ shanks.py
@@ -0,0 +1,34 @@
+import math
+import numpy as np
+
+def get_order(root: int, mod: int) -> int:
+    for i in range(1, mod):
+        if root**i % mod == 1:
+            return i
+        
+def get_inverse(num: int, mod: int) -> int:
+    num %= mod
+    if math.gcd(num, mod) == 1:
+        for i in range(1, mod):
+            if (num * i) % mod == 1:
+                return i
+    else:
+        return None
+
+def baby_step_giant_step(root: int, mod: int, result: int) -> int:
+    order = get_order(root, mod)
+    if order:
+        ## Step 1: Calculate n
+        n = 1 + math.floor(np.sqrt(order))
+        ## Step 2: Create the lists of powers
+        list_a = [(root**pow) % mod for pow in range(n+1)]
+        list_b = [(result*get_inverse((root**(k*n)), mod)) % mod for k in range(n+1)]
+        ## Step 3: Find a match between the two lists
+        match = next(iter(set(list_a) & set(list_b)))
+        ## Step 4: Return the solution
+        if match:
+            return (list_a.index(match) + list_b.index(match)*n) % (mod-1)
+        else:
+            return None
+    
+print(baby_step_giant_step(156, 593, 116))
---
+++ shanks_test.py
@@ -0,0 +1,24 @@
+import shanks
+
+class TestSolution():
+    def test_get_order(self):
+        assert shanks.get_order(5, 7) == 6
+        assert shanks.get_order(2, 3) == 2
+        assert shanks.get_order(3, 7) == 6
+        assert shanks.get_order(2, 5) == 4
+        assert shanks.get_order(4, 5) == 2
+        assert shanks.get_order(3, 5) == 4
+
+    def test_get_inverse(self):
+        assert shanks.get_inverse(4, 5) == 4
+        assert shanks.get_inverse(2, 2) == None
+        assert shanks.get_inverse(5, 6) == 5
+        assert shanks.get_inverse(1, 2) == 1
+        assert shanks.get_inverse(36, 71) == 2
+
+    def test_baby_step_giant_step(self):
+        assert shanks.baby_step_giant_step(11, 71, 21) == 37
+        assert shanks.baby_step_giant_step(3, 7, 5) == 5
+        assert shanks.baby_step_giant_step(3, 7, 4) == 4
+        assert shanks.baby_step_giant_step(5, 23, 8) == 6
+        assert shanks.baby_step_giant_step(156, 593, 116) == 59
